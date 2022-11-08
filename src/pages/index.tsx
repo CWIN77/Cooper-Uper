467f6e06ac63e36a3cd667816b54cc3d0d93451a
CWIN77 <cwstbp0426@gmail.com>
2022-11-08 19:05:27 +0900

    gitignore 수정, 페이지 초기화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components'
-import SvgHeart from '../svg/heart.svg';
 
 export default function Home() {
   return (
     <Container>
-      <h1>My Next WebApp</h1>
-      <SvgHeart width="24" height="24" fill="white" />
+      
     </Container>
   )
 }
