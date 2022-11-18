10b9e1b473d20177501a131c465d4c64c9fdd2e7
CWIN77 <cwstbp0426@gmail.com>
2022-11-18 23:18:24 +0900

    서버 api 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
 import styled from 'styled-components'
 import Header from '../components/header'
 export default function Home() {
+  const getData = () => {
+    fetch("https://express-mongodb-eta.vercel.app/goal")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  }
+  const createUser = () => {
+    fetch("http://localhost:9000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    }).then((response) => console.log(response));
+  }
   return (
     <Container>
       <Header />
+      <button onClick={getData}>데이터 가져오기</button>
+      <button onClick={createUser}>유저 만들기</button>
     </Container>
   )
 }
